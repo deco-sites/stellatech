9b9c23cd3e8348e5050414a9a143fc7bc74f2f34
decobot <capy@deco.cx>
2024-08-23 00:32:45 -0300

    feat: add mobile styles

--- sections/HeaderHero.tsx
+++ sections/HeaderHero.tsx
@@ -27,7 +27,7 @@ export default function HeaderHero({ backgroundImage, description, image, items,
         <div
             class="h-[12%] lg:h-1/2 border border-white absolute top-0 left-[70%] lg:left-[43%] z-20"
         />
-        <div class="relative top-0 left-0 w-full h-[90%] lg:h-full z-10 flex items-start lg:items-center rounded-[34px] pl-6"
+        <div class="relative top-0 left-0 w-full h-full z-10 flex items-start lg:items-center rounded-[34px] pl-6"
             style={{
                 background: 'linear-gradient(252.56deg, rgba(154, 30, 133, 0.97) 19.32%, rgba(249, 157, 15, 0.97) 78.9%, rgba(255, 203, 79, 0.97) 104.97%)'
             }}
@@ -35,11 +35,11 @@ export default function HeaderHero({ backgroundImage, description, image, items,
           <Icon class="hidden lg:flex mb-[15%]" id="Logo" width={336} />
           <Icon class="lg:hidden mb-[15%]" id="Logo" width={163} height={100} />
         </div>
-        <div class="absolute bottom-[15%] z-20 max-w-[300px] lg:max-w-[500px] left-[10%] lg:left-[25%]">
+        <div class="absolute bottom-[5%] lg:bottom-[13%] z-20 max-w-[300px] lg:max-w-[500px] left-[8%] lg:left-[25%]">
             <p class="text-base lg:text-2xl leading-8 lg:leading-[34px] text-white pb-4 lg:pb-0">{description}</p>
             <h1 class="text-[38px] lg:text-[55px] leading-[32px] lg:leading-[83.66px] text-white font-black uppercase">{title}</h1>
         </div>
-        <div class="overflow-x-auto bg-primary w-[90%] mx-auto rounded-[21px] relative z-20 lg:bottom-[8%] flex justify-between px-5">
+        <div class="overflow-x-auto bg-primary w-full lg:w-[90%] mx-auto rounded-[21px] relative z-20 bottom-0 lg:bottom-[7%] flex justify-between px-5">
             {
               items.map(item => (
                 <div class="flex gap-2 justify-center items-center py-5 min-w-[200px]">
